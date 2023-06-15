316a2e2ca91af696538dbebc2e7a941d6cda635c
kakoliislam <132891352+kakoliislam@users.noreply.github.com>
2023-06-15 14:04:46 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,20 @@
+var icon=document.getElementById("bars");
+icon.addEventListener("click",function(){
+	var menu=document.getElementById("menu-items");
+	menu.classList.toggle("active");
+})
+
+
+var menuItems=document.getElementById("menu-items");
+menuItems.addEventListener("click",function(){
+	var menu=document.getElementById("menu-items");
+	menu.classList.remove("active");
+	
+	
+	
+	
+})
+
+
+
+
